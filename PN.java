e55dac925b78369ba374b9dd25fb02d16be34d6a
Priyanshi Kanpariya <Priyanshi Kanpariya>
2025-08-05 18:29:28 +0530

    new

---
+++ PN.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PN {
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a > 0){
+            System.out.println("oddd");
+        }else{
+            System.out.println("evenn");
+        }
+    }
+    
+}
