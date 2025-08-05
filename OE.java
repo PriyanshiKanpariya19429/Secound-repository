f3d99ff52feeab99e62dd149fa376ea10ce22179
Priyanshi Kanpariya <Priyanshi Kanpariya>
2025-08-05 15:26:38 +0530

    neww

--- OE.java
+++ OE.java
@@ -3,11 +3,11 @@ public class OE{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         int a =sc.nextInt();
-        if(a % 2 == 0){
-            System.out.println("This number is even");
+        if(a>0){
+            System.out.println("This number is positive");
         }
         else{
-            System.out.println("This number is odd");
+            System.out.println("This number is nagative");
         }
         }
     }
